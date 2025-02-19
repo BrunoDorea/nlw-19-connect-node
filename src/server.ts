7039bff10b04234d6f7c768f06506f3e27670143
Bruno <brunodorea@outlook.com.br>
2025-02-19 13:34:07 -0300

    :wrench: config: ajustes na config para deploy

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import { fastifyCors } from "@fastify/cors"
 import { fastifySwagger } from "@fastify/swagger"
 import { fastifySwaggerUi } from "@fastify/swagger-ui"
 import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod"
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
+import { getRankingRoute } from "./routes/get-ranking-route"
 import { accessInviteLinkRoute } from "./routes/access-invite-link-route"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route"
 import { getSubscriberInviteClicksRoute } from "./routes/get-subscriber-invite-clicks-route"
 import { getSubscriberInvitesCountRoute } from "./routes/get-subscriber-invites-count-route"
 import { getSubscriberRankingPositionRoute } from "./routes/get-subscriber-ranking-position-route"
-import { getRankingRoute } from "./routes/get-ranking-route"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
@@ -39,10 +39,6 @@ app.register(getSubscriberInvitesCountRoute)
 app.register(getSubscriberRankingPositionRoute)
 app.register(getRankingRoute)
 
-const baseUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${env.PORT}`
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
     console.log("HTTP Server Running!")
-    console.log(`DOCS -> ${baseUrl}/docs`)
-    console.log(`POSTGRESQL -> ${env.POSTGRES_URL}`)
-    console.log(`REDIS -> ${env.REDIS_URL}`)
 })
